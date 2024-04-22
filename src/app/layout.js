005f7543a8c55cbf4e80cae8b70642163ee95bde
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-04-22 12:07:20 +0530

    initial setup

---
+++ src/app/layout.js
@@ -0,0 +1,13 @@
+import "./globals.css"; // Assuming this is your global CSS file
+
+//for slider
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
