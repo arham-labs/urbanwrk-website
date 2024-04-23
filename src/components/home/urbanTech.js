0ad21bf8fb154a259abce74655e48570d54f60c3
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-04-23 16:59:53 +0530

    feat: home page

---
+++ src/components/home/urbanTech.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function UrbanTech() {
+    return (
+        <div className='bg-[#F6F4EE]'>
+            <div className='w-[88%] mx-auto py-20'>
+                <div className='flex items-center'>
+                    <div className='w-1/2 aspect-[38/39]'>
+                        <Image src="/images/home/aboutUs.png" height={666} width={683} alt="aboutUs" />
+                    </div>
+                    <div className='w-1/2 flex flex-col justify-center pl-12'>
+                        <div>
+                        <p>URBANWRK TECH</p>
+                            <h1 className='font-medium text-6xl pb-8'>Integrate the next gen of work</h1>
+                            <p className='text-base pb-4'>Get the best of managed workspace solutions for your owned or leased real estate. Optimise costs, give employees the space to collaborate and the tools to work from more offices across multiple cities.</p>
+                            <div className='pt-7'>
+                                <button className='py-2 px-[40px] bg-dark text-white'>Book a Demo</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
