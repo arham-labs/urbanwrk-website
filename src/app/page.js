ec60d616efdf8f64d89637963064185d1c52648c
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-04-23 17:20:00 +0530

    fix: home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
-import Solutions from "@/components/home/Solutions";
+import Solutions from "@/components/home/solutions";
 import AboutCard from "@/components/home/aboutCard";
 import BuildCard from "@/components/home/buildCard";
+import UrbanTech from "@/components/home/urbanTech";
 
 export default function Home() {
   return (
     <>
       <AboutCard />
-      <Solutions />
+      {/* <Solutions /> */}
       <BuildCard/>
+      <UrbanTech/>
     </>
   );
 }
