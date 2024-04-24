94db9fb71ad4c66e2ca77fe4bcd71476c5eb7cf6
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-04-24 11:41:53 +0530

    fix : home page

--- src/components/home/solutionCard.js
+++ src/components/home/solutionCard.js
@@ -1,7 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function SolutionCard() {
   return (
-    <div>solutionCard</div>
+    <div className='w-[422px] h-[557px] bg-[#F6F4EE]'>
+
+      <div className='flex flex-col justify-between'>
+        <div>
+          <Image src="/images/home/solutionCard1.png" height={400} width={422} alt='SolutionCard' />
+        </div>
+        <div>
+          <div className='flex justify-between items-center pb-3 pt-4 px-5'>
+            <div>
+              <div className='text-[32px] leading-[1.2]'>Bespoke</div>
+              <div className='text-[32px] leading-[1.2]'>Office</div>
+            </div>
+            <div className='flex items-center'>
+              <div className=''>Explore</div>
+              <Image src="/images/home/explore.svg" height={24} width={18} alt='exploreSvg' className='ml-1' />
+            </div>
+          </div>
+          <div className='px-5'>
+            <div className='border-black border'></div>
+          </div>
+          <p className='text-base px-5 pt-2 pb-5'>Fully-managed, branded solution built as per your vision, within 45-60 days.</p>
+        </div>
+      </div>
+      <div className='bg-primary h-3 border border-primary mt-1'></div>
+    </div>
   )
 }
--- src/components/home/solutions.js
+++ src/components/home/solutions.js
@@ -1,15 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
+import SolutionCard from './solutionCard'
+import SolutionSlider from './solutionsSlider'
 
 export default function Solutions() {
     return (
         <div>
-            <h1>Solutions</h1>
-            <h2>for everything you need</h2>
-            <div>
-                <Image src="/images/home/aboutUs.png" height={666} width={683} alt="aboutUs" />
+            <div className='bg-white'>
+                <div className='text-center'>
+                    <div className='font-medium text-[65px] pb-0'>Solutions</div>
+                    <div className='font-kepler text-[65px] text-center pt-0'>for everything you need</div>
+                </div>
+                <div className='w-[88%] mx-auto py-20'>
+                    <SolutionSlider />
+                    <SolutionCard />
+                </div>
             </div>
-            <></>
+
         </div>
     )
 }
