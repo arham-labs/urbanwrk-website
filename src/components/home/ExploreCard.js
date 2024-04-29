e75efc91e40dbf882a46307df0c53f845574458e
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-04-29 17:45:31 +0530

    feat: home page

--- src/components/home/ExploreCard.js
+++ src/components/home/ExploreCard.js
@@ -11,11 +11,11 @@ export default function ExploreCard() {
                 <div>
                     <Image src="/images/home/exploreCard3.png" height={836} width={422} alt="exploreCard" className='h-[100vh] w-full object-top' unoptimized />
                 </div>
-                <div className='absolute top-20 inset-x-0 flex justify-center items-center font-medium text-[65px] max-md:text-[28px]'>
+                <div className='absolute top-28 inset-x-0 flex justify-center items-center font-medium text-[65px] max-md:text-[28px]'>
                     <div >What are you looking for?</div>
                 </div>
-                <div className='absolute bottom-60 inset-x-0 flex justify-center items-center font-medium text-[65px]'>
-                    <p className='text-[30px] py-20 text-white w-[40%] text-center max-md:text-[20px]'>
+                <div className='absolute md:bottom-60 bottom-96  inset-x-0 flex justify-center items-center font-medium text-[65px]'>
+                    <p className='text-[30px] md:py-20 text-white md:w-[40%] w-full  text-center max-md:text-[20px]'>
                         Leave your mark with office spaces in cities that are,
                         <br className="lg:hidden" />
                         shaping the economy&apos;s next chapter.
@@ -23,7 +23,7 @@ export default function ExploreCard() {
 
                 </div>
 
-                <div className='absolute bottom-16 inset-x-0 flex justify-center items-center font-medium text-[65px] px-5 '>
+                <div className='absolute bottom-24 inset-x-0 flex justify-center items-center font-medium text-[65px] px-5 '>
                     <div className='bg-bglight1 px-10 gap-10 flex-wrap py-4 relative flex justify-between items-center max-md:flex-col'>
                         <div className='flex-grow flex items-center justify-center'>
                             <BasicSelect />
