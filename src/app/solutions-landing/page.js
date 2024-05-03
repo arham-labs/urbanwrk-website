f099e5b34840203a9bd768f35b492a5e9d1deb55
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-05-03 16:42:33 +0530

    fix: location-inside

--- src/app/solutions-landing/page.js
+++ src/app/solutions-landing/page.js
@@ -10,6 +10,9 @@ import React from 'react'
 import SolutionBanner from '@/components/solutionsLanding/solutionBanner'
 import CompareSolution from '@/components/solutionsLanding/compareSolution';
 import Solutions from '@/components/solutionsLanding/solutions';
+import Testimonial from '@/common/testimonial';
+import GoodCompany from '@/components/home/goodCompany';
+import FormCard from '@/components/home/formCard';
 
 export default function page() {
     return (
@@ -35,6 +38,21 @@ export default function page() {
             </div>
 
             <ExploreCard />
+            <div className="bg-bglight4 ">
+                <div className=" px-6 max-w-[1920px] py-20  mx-auto 2xl:px-[160px] lg:px-[120px]">
+                    <Testimonial />
+                </div>
+            </div>
+            <div className="bg-bglight1 ">
+                <div className=" px-6 max-w-[1920px] py-20  mx-auto 2xl:px-[160px] lg:px-[120px]">
+                    <GoodCompany />
+                </div>
+            </div>
+            <div className="bg-white ">
+                <div className=" px-6 max-w-[1920px] py-20  mx-auto 2xl:px-[160px] lg:px-[120px]">
+                    <FormCard />
+                </div>
+            </div>
             {/* <Reviews /> */}
             {/* <Partners /> */}
         </>
