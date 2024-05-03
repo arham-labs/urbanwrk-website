6d332e0ac792d47d4d90af47ee8cf8fe90b48335
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-05-03 15:42:50 +0530

    fix: mobile view

--- src/components/locationInside/locationParkSliderMobile.js
+++ src/components/locationInside/locationParkSliderMobile.js
@@ -1,9 +1,7 @@
 "use client"
 import React from 'react'
 import Slider from "react-slick";
-import LocationSpaceCard from './locationSpaceCard';
 import LocationSpaceCardMobile from './locationSpaceCardMobile';
-
 
 export default function LocationParkSliderMobile() {
     const settings = {
@@ -16,30 +14,34 @@ export default function LocationParkSliderMobile() {
         autoplaySpeed: 1500,
     };
     return (
-        <div className="slider-container">
+        <div className="slider-container relative">
             <Slider {...settings}>
-
                 {[...Array(3)].map((item, index) => (
-                    <div key={index} className="mb-6 " >
+                    <div key={index} className="mb-12">
                         <LocationSpaceCardMobile />
                     </div>
                 ))}
-
-
             </Slider>
             <style jsx global>{`
-              
+                .slick-dots {
+                    text-align: center !important;
+                    bottom: 20px;
+                    width: 100%;
+                    padding: 0;
+                }
+                .slick-dots li {
+                    display: inline-block;
+                    margin: 0 5px;
+                }
                 .slick-dots li button:before {
                     color: #7D7C7C !important;
                     font-size: 8px !important;
                 }
-                .slick-dots li.slick-active button:before   {
+                .slick-dots li.slick-active button:before {
                     color: #C72030 !important;
                     font-size: 8px !important;
                 }
             `}</style>
         </div>
-
     );
-
 }
