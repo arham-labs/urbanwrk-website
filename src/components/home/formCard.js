905289c31fb54c720bd2fd6363ee74c7c2d1ce8f
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-05-05 10:38:58 +0530

    fix: qc changes

--- src/components/home/formCard.js
+++ src/components/home/formCard.js
@@ -14,15 +14,15 @@ export default function FormCard() {
 
     return (
         <div>
-            <div className='flex flex-col md:flex-row  md:gap-14 lg:gap-16  xl:gap-24 2xl:gap-28 '>
-                <div className='w-[604px] h-[650px] max-md:w-full max-md:h-[500px]  order-1 sm:order-1'>
+            <div className='flex flex-col md:flex-row  md:gap-14 lg:gap-16  xl:gap-24 2xl:gap-32 '>
+                <div className='md:w-[604px]  lg:w-[640px] 2xl:w-[680px] md:h-[650] xl:h-[660px]  max-md:w-full max-md:h-[500px]  order-1 sm:order-1'>
                     <Image src="/images/home/form.png" height={800} width={604} alt="formImage" className='h-full w-full' />
                 </div>
                 <div className=' flex flex-col   order-1 sm:order-2 max-md:pt-[48px]'>
                     <h2 className='text-2xl font-medium  md:text-4xl md:pb-2'>Always find your space</h2>
                     <h2 className='text-2xl md:text-5xl font-kepler'>in the city of your choice.</h2>
                     <p className='py-4 max-md:py-6'>Fill in the details below or call us at +91 98765 43210</p>
-                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col md:w-[350px] lg:w-[462px] md:mt-4">
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col md:w-[350px] lg:w-[400px] xl:w-[462px] md:mt-4">
                         <div className='mb-4'>
                             <label className='block font-semibold'>Name:</label>
                             <input type="text" {...register("name", { required: true })} className="border-black border-solid border w-full h-[38px]" />
@@ -59,12 +59,7 @@ export default function FormCard() {
                             <input type="checkbox" {...register("newsUpdates")} />
                             <label className='ml-2 text-sm '>{`I'd like to receive the latest news and updates from UrbanWrk.`}</label>
                         </div>
-                        {/* <div className='border-black border flex px-6 my-1 h-[36px] text center '>
-
-                            <div className='text-base bg-transparent text-center  py-1 text-black '>Submit
-                            </div>
-                            <Image src="/images/home/btnArrow.svg" height={12} width={12} alt='exploreSvg' className='ml-1' />
-                        </div> */}
+                        
                         <div className='w-full pt-2'>
                             <ExploreBtn btnName={"submit"} />
                         </div>
