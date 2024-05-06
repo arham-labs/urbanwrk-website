6a072aca6eac25ef719c95ffe5f9de4aab1ab165
Jaykrishna <jaykrishnanandagiri@arhamlabs.com>
2024-05-06 18:20:17 +0530

    refactor: blogs images aspect ratio change

--- src/common/blog.js
+++ src/common/blog.js
@@ -15,7 +15,7 @@ export default function Blog() {
                         <div className="relative">
                             <span className="absolute top-4 left-5 lg:top-6 lg:left-8 uppercase bg-bglight1 p-2 lg:p-3 text-xs font-semibold lg:text-sm">Workspace Solutions</span>
                             <Image src={Blog2} alt="blog-1" className="hidden lg:block aspect-[5/4] w-full" />
-                            <Image src={Blog2Mobile} alt="blog-1" className="aspect-square lg:hidden w-full" />
+                            <Image src={Blog2Mobile} alt="blog-1" className="lg:hidden w-full" />
                         </div>
                         <div className="w-full lg:w-[85%] flex flex-col gap-2 lg:gap-4">
                             <span className="text-lg lg:text-2xl leading-6 lg:leading-8 font-semibold">How Flexible Workspaces can meet the demands of today and tomorrow</span>
@@ -26,7 +26,7 @@ export default function Blog() {
                         <div className="relative">
                             <span className="absolute top-4 left-5 lg:top-6 lg:left-8 uppercase bg-bglight1 p-2 lg:p-3 text-xs font-semibold lg:text-sm">Resources</span>
                             <Image src={Blog1} alt="blog-2" className="hidden lg:block aspect-[26/35] w-full" />
-                            <Image src={Blog1Mobile} alt="blog-2" className="aspect-square lg:hidden w-full" />
+                            <Image src={Blog1Mobile} alt="blog-2" className="lg:hidden w-full" />
                         </div>
                         <div className="w-full lg:w-[85%] flex flex-col gap-2 lg:gap-4">
                             <span className="text-lg lg:text-2xl leading-6 lg:leading-8 font-semibold">Why planning your office space is a necessity?</span>
