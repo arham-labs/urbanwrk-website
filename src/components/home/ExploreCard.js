e008bb811f272c535fdb09b02ccd9a6eb27af321
Arjunsingh Rajput <arjunrajput@arhamlabs.com>
2024-05-14 16:58:40 +0530

    fix: qc change dropdown

--- src/components/home/ExploreCard.js
+++ src/components/home/ExploreCard.js
@@ -1,49 +1,54 @@
-import Image from 'next/image'
-import React from 'react'
-import Dropdown from './dropdown'
-import BasicSelect from './dropdown'
-import LookingImageSlider from './lookingImageSlider'
-import ExploreBtn from './exploreBtn'
-
+import Image from "next/image";
+import React from "react";
+import Dropdown from "./dropdown";
+import BasicSelect from "./dropdown";
+import LookingImageSlider from "./lookingImageSlider";
+import ExploreBtn from "./exploreBtn";
+import BasicSelectDrop from "../select";
 
 export default function ExploreCard() {
-    const city = ["Mumbai", "Pune", "Nagpur"]
-    const solution = ["Tailormade Offices", "Ready-To-Work In Offices", "Meeting Rooms"]
-    return (
+  const city = ["Mumbai", "Pune", "Nagpur"];
+  const solution = [
+    "Tailormade Offices",
+    "Ready-To-Work In Offices",
+    "Meeting Rooms",
+  ];
+  return (
+    <div>
+      <div className="relative h-[800px] md:h-[875px] ">
         <div>
-            <div className='relative h-[800px] md:h-[875px] '>
-                <div>
-                    {/* <Image src="/images/home/exploreCard3.png" height={836} width={422} alt="exploreCard" className='h-[100vh] w-full object-top' unoptimized /> */}
-                    <LookingImageSlider />
-                </div>
-                <div className='absolute  top-14   md:top-20 inset-x-0 flex justify-center items-center font-medium text-6xl max-md:text-[28px] max-md:tracking-[-0.56px]'>
-                    <div >What are you <span className='font-kepler max-md:text-[28px] text-6xl '>
-                        looking for?
-                    </span></div>
-                </div>
-                <div className='absolute md:bottom-40  bottom-80    inset-x-0 flex justify-center items-center font-medium text-[65px]'>
-                    <p className='text-[30px] md:py-20 text-white md:w-[753px]  text-center max-md:text-[20px] max-md:mx-4 max-md:leading-[30px]'>
-                        Leave your mark with office spaces in cities that are,
-
-                        shaping the economy&apos;s next chapter.
-                    </p>
-
-                </div>
-
-                <div className='absolute bottom-16  md:bottom-20 inset-x-0 flex justify-center items-center font-medium text-[65px] px-5  '>
-                    <div className='px-10 bg-bglight1 max-md:w-full gap-10 flex-wrap py-4 relative flex justify-between  max-md:flex-col max-md:px-5 max-md:gap-6 max-md:py-6 '>
-                        <div className='flex-grow flex items-center justify-center'>
-                            <BasicSelect labelName="City" list={city} />
-                        </div>
-                        <div className='flex-grow flex items-center justify-center'>
-                            <BasicSelect labelName="Solution" list={solution} />
-                        </div>
-                        <div>
-                            <ExploreBtn btnName={"Explore"} />
-                        </div>
-                    </div>
-                </div>
-            </div>
+          {/* <Image src="/images/home/exploreCard3.png" height={836} width={422} alt="exploreCard" className='h-[100vh] w-full object-top' unoptimized /> */}
+          <LookingImageSlider />
         </div>
-    )
+        <div className="absolute  top-14   md:top-20 inset-x-0 flex justify-center items-center font-medium text-6xl max-md:text-[28px] max-md:tracking-[-0.56px]">
+          <div>
+            What are you{" "}
+            <span className="font-kepler max-md:text-[28px] text-6xl ">
+              looking for?
+            </span>
+          </div>
+        </div>
+        <div className="absolute md:bottom-40  bottom-80    inset-x-0 flex justify-center items-center font-medium text-[65px]">
+          <p className="text-[30px] md:py-20 text-white md:w-[753px]  text-center max-md:text-[20px] max-md:mx-4 max-md:leading-[30px]">
+            Leave your mark with office spaces in cities that are, shaping the
+            economy&apos;s next chapter.
+          </p>
+        </div>
+
+        <div className="absolute bottom-16  md:bottom-20 inset-x-0  font-medium text-[65px] px-5 max-w-[850px] mx-auto ">
+          <div className="px-10 bg-bglight1 max-md:w-full gap-10 flex-wrap py-4 relative flex justify-between   md:items-center items-start max-md:flex-col max-md:px-5 max-md:gap-6 max-md:py-6 ">
+            <div className="basis-[100%] md:basis-[30%] sm:basis-[100%] w-full mx-[-10px]">
+              <BasicSelectDrop list={city} labelName="City" />
+            </div>
+            <div className="basis-[100%] md:basis-[30%] sm:basis-[100%] w-full mx-[-10px]">
+              <BasicSelectDrop list={solution} labelName="Solution" />
+            </div>
+            <div className="basis-[100%] md:basis-[15%] sm:basis-[100%] ">
+              <ExploreBtn btnName={"Explore"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/select.js
@@ -0,0 +1,89 @@
+"use client";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const BootstrapInput = styled(InputBase)(({ theme, selectedOption,fullWidth }) => ({
+  // {console.log(age)}
+  "label + &": {
+    marginTop: theme.spacing(3),
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    width: "100%",
+    padding: "5px 36px 5px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    // Use the system font instead of the default Roboto font.
+
+    "&:focus": {
+      outline: "none",
+      border: "1px solid #ced4da",
+      boxShadow: "none",
+    },
+  },
+  "& .MuiSelect-icon": {
+    color: selectedOption ? "#C72030" : "",
+    fontSize: "20px",
+    marginRight: "10px",
+  },
+}));
+
+export default function CustomizedSelects({ list, labelName }) {
+  const [selectedOption, setSelectedOption] = React.useState("");
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div className="w-full h-[50px] ">
+      <FormControl sx={{ m: 1, width:"100%" }} variant="standard">
+        <Select
+          labelId="demo-customized-select-label"
+          id="demo-customized-select"
+          value={selectedOption}
+          onChange={handleChange}
+          input={<BootstrapInput fullWidth selectedOption={selectedOption} />}
+          displayEmpty
+          fullWidth
+          IconComponent={KeyboardArrowDownIcon}
+          renderValue={(selected) => {
+            if (selected.length === 0) {
+              return <p className="text-[#A5A5A5]">{labelName}</p>;
+            }
+            return selected;
+          }}
+          inputProps={{ "aria-label": "Without label" }}
+          MenuProps={{
+            anchorOrigin: {
+              vertical: "bottom",
+              horizontal: "left",
+            },
+            transformOrigin: {
+              vertical: "top",
+              horizontal: "left",
+            },
+            getContentAnchorEl: null,
+          }}
+        >
+          {list.map((item, i) => (
+            <MenuItem
+              value={item}
+              key={i}
+              className={selectedOption === item ? "text-primary !bg-white" : "hover:bg-red-300"}
+            >
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
