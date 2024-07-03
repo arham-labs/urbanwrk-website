e29df1301da344e635fed122e012a5d8e00ca189
Arjun Rajput <arjunrajput@arhamlabs.com>
2024-07-03 17:58:30 +0530

    fix: chat

--- src/components/chat.js
+++ src/components/chat.js
@@ -168,12 +168,12 @@ export default function Chat() {
                 <button
                   type="submit"
                   disabled={isLoading}
-                  className={`border-black border  group-hover:border-none px-4 gap-2 flex items-center h-[36px] w-fit transition-all  ease-in-out group-hover:bg-primary group-hover:text-white cursor-pointer ${
+                  className={`border-black border w-full justify-center   group-hover:border-none  max-lg:px-0 px-4 gap-2 flex items-center h-[36px] transition-all  ease-in-out group-hover:bg-primary group-hover:text-white cursor-pointer ${
                     isLoading ? "bg-gray-300 cursor-not-allowed" : ""
                   }`}
              
                 >
-                  <span className="text-base bg-transparent  max-md:px-4 flex py-1 text-black group-hover:text-white">
+                  <span className="text-base bg-transparent max-lg:px-0  flex py-1 text-black group-hover:text-white ">
                    Submit </span>
                    <div className="bg-[url('/images/home/btnArrow.svg')] group-hover:bg-[url('/images/home/lightArrow.svg')] bg-contain w-[14px] h-[14px] bg-no-repeat ">
 
