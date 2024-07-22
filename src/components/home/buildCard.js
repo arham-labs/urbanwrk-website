ec516c11b909f903cec689f4571c30602de80ed3
Jaykrishna <jaykrishnanandagiri@arhamlabs.com>
2024-07-22 17:29:36 +0530

    refactor: qc changes add

--- src/components/home/buildCard.js
+++ src/components/home/buildCard.js
@@ -13,7 +13,7 @@ export default function BuildCard() {
                             <h2 className='font-normal text-2xl lg:text-4xl xl:text-5xl 2xl:text-6xl  flex flex-wrap '>Build something</h2>
                             <h2 className='font-normal text-[32px] lg:text-[44px] xl:text-[56px] 2xl:text-[68px] pb-6 font-kepler leading-[1.2]  '> meaningful</h2>
                             <div className='flex  items-center gap-[10px]'>
-                                <Image src="images/home/build.svg" alt="build" height={42} width={43} />
+                                <Image src="/images/home/build.svg" alt="build" height={42} width={43} />
                                 <p className='text-base font-medium '>India’s first & only IGBC Platinum rated office spaces.</p>
                             </div>
                             <p className='text-base py-4'>When it comes to productivity, we put the planet first. Why? Because research has proven that a work environment that is aligned with its natural environment makes for better, more positive outcome.</p>
