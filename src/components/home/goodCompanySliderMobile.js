dc6524166bf50e3ae29687a5a4685881e8db3346
Jaykrishna <jaykrishnanandagiri@arhamlabs.com>
2024-07-25 15:13:00 +0530

    refactor: logo added and slick text removed

--- src/components/home/goodCompanySliderMobile.js
+++ src/components/home/goodCompanySliderMobile.js
@@ -12,21 +12,23 @@ export default function GoodCompanySliderMobile() {
         slidesToScroll: 2,
         autoplaySpeed: 2000,
         rows: 2,
-        arrows:false
+        arrows: false
     };
 
     return (
-        <Slider {...settings}>
-            {[...Array(24)].map((_, i) => (
-                <div className='w-1/2 my-5 flex flex-col justify-around' key={i}>
-                    <div className='flex justify-center items-center h-[100px] w-[100px] mx-auto'>
-                        <Image src={`/images/home/logooo${i + 1}.svg`} height={50} width={60} alt="logo" className='w-full' />
+        <>
+            <Slider {...settings}>
+                {[...Array(24)].map((_, i) => (
+                    <div className='w-1/2 my-5 flex flex-col justify-around' key={i}>
+                        <div className='flex justify-center items-center h-[100px] w-[100px] mx-auto'>
+                            <Image src={`/images/home/logooo${i + 1}.svg`} height={50} width={60} alt="logo" className='w-full' />
+                        </div>
+                        <div className='pt-8'>
+                            <div className='border-rgba(116, 116, 116, 0.60) border w-[150px] mx-auto'></div>
+                        </div>
                     </div>
-                    <div className='pt-8'>
-                        <div className='border-rgba(116, 116, 116, 0.60) border w-[150px] mx-auto'></div>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </Slider>
             <style>{`
             .slick-dots li.slick-active button:before {
                 color: #C72030 !important;
@@ -35,6 +37,7 @@ export default function GoodCompanySliderMobile() {
                 font-size: 8px !important;
             }
             `}</style>
-        </Slider>
+        </>
+
     );
 }
