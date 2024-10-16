d8edd4301ea799418026c52afa7dc930e9f1fb51
Jaykrishna <jaykrishnanandagiri@arhamlabs.com>
2024-10-16 13:18:45 +0530

    feat: home page video and poster add

--- src/components/home/homeBanner.js
+++ src/components/home/homeBanner.js
@@ -1,30 +1,55 @@
-import Image from 'next/image'
-import BannerImage from '../../../public/images/home/homeBanner1.webp'
+'use client'
+import React, { useRef, useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function HomeBanner() {
+    const [showIcon, setShowIcon] = useState(true);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [poster, setPoster] = useState('/images/home/home-banner-poster.png'); // default for web
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const updatePoster = () => {
+            if (window.innerWidth <= 768) {
+                setPoster('/images/home/mobile-banner-poster.png'); // mobile poster
+            } else {
+                setPoster('/images/home/home-banner-poster.png'); // web poster
+            }
+        };
+
+        // Set poster on initial render
+        updatePoster();
+
+        // Update poster on window resize
+        window.addEventListener('resize', updatePoster);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', updatePoster);
+        };
+    }, []);
+
+    const handlePlay = () => {
+        setIsPlaying(!isPlaying);
+        if (!isPlaying) {
+            videoRef.current.play();
+            setShowIcon(false);
+        } else {
+            videoRef.current.pause();
+            setShowIcon(true);
+        }
+    };
+
     return (
-        <>
-            <div className='h-screen hidden lg:block'>
-                <Image
-                    src={BannerImage}
-                    alt="UrbanWrk work space"
-                    className='h-full w-full object-cover'
-                    priority
-                    layout='fill' // Ensures the image covers the container properly
-                    unoptimized
-                />
-            </div>
-            <div className='lg:hidden h-screen'>
-                <Image
-                    src="/images/home/mobile-banner1.webp"
-                    alt="UrbanWrk work space"
-                    height={375}
-                    width={720}
-                    className='h-full w-full object-cover'
-                    priority
-                    unoptimized
-                />
-            </div>
-        </>
-    )
+        <div className="flex bg-cover bg-no-repeat relative h-screen cursor-pointer" onClick={handlePlay}>
+            <video ref={videoRef} loop playsInline poster={poster} className="absolute right-0 left-0 top-0 w-full h-full object-cover back-video bottom-0 -z-[1]">
+                <source src="/videos/home-video.mp4" type="video/mp4" />
+            </video>
+            {showIcon && (
+                <div className='absolute top-1/2 left-1/2'>
+                    <Image src="/images/locationInside/play-icon.svg" alt="play" width={100} height={100} className='w-8 lg:w-14' />
+                </div>
+            )}
+        </div>
+    );
 }
